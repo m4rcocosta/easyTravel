bee3b757e96b8ef783da4c41871bcad4e533ae20
Gianluca Pulicati <pulicati.1708686@studenti.uniroma1.it>
2018-10-29 22:31:48 +0100

    Fixed default Statistic value

--- db/migrate/20181029140728_create_statistics.rb
+++ db/migrate/20181029140728_create_statistics.rb
@@ -1,7 +1,7 @@
 class CreateStatistics < ActiveRecord::Migration[5.2]
   def change
     create_table :statistics do |t|
-      t.integer :conteggio, null: false, default: 0
+      t.integer :conteggio, null: false, default: 1
       t.string :partenza, null: false, default: ""
       t.string :arrivo, null: false, default: ""
 
