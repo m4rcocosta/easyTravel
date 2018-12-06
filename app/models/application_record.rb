075f699adaf645c31e2cda238541d62f0fe0997a
Marco Costa <costa.1691388@studenti.uniroma1.it>
2018-12-06 16:39:36 +0100

    Removed keys

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
-  require 'open_weather'
-
-    options = { units: "metric", APPID: "07a22ac8fa1aeeedcfec8bc055cbe3eb" }
-    
+  self.abstract_class = true    
 end
